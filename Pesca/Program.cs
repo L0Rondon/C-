6df59a0a32a1f9008998c5b4ba5bf25f996c339c
L0Rondon <lrondon2008@gmail.com>
2025-02-10 17:17:02 -0300

    Atividade-Pesca

---
+++ Pesca/Program.cs
@@ -0,0 +1,27 @@
+﻿int[] Lago = new int[50];
+int qtdPeixes, qtdPessoas, qtdIscas;
+string[] nomes;
+Console.Write("Quantos Peixes tem no lago: ");
+qtdPeixes = int.Parse(Console.ReadLine());
+Console.Write("Número de Jogadores: ");
+qtdPessoas = int.Parse(Console.ReadLine());
+Console.Write("Numéro de Iscas/Tentativas: ");
+qtdIscas = int.Parse(Console.ReadLine());
+nomes = new string[qtdPessoas];
+for(int i =0;i<qtdPessoas;i++)
+{
+    Console.Write("Escreva o nome da " + (i + 1) + "° pessoa: ");
+    nomes[i] = Console.ReadLine();
+}
+Console.WriteLine("Escolha a posição:"); 
+    for(int i = 1; i <= 25; i++)
+{
+    Console.Write("( "+i+")");
+}
+Console.WriteLine();
+for (int i = 25; i <= 50; i++)
+{
+    Console.Write("(" + i + ")");
+}
+
+
