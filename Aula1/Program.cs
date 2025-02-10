997b0b050e145fa482cba74af4931be13cf69f8d
SENAI Zerbini <aluno@escolar509.sp.senai.br>
2025-02-10 14:41:51 -0300

    Aula1

---
+++ Aula1/Program.cs
@@ -0,0 +1,70 @@
+﻿//Exercicio 1
+
+/*
+const double imposto = 0.21;
+double ValorSemImposto = 23.5;
+double resultado;
+
+resultado = imposto * ValorSemImposto;
+Console.WriteLine("Valor sem imposto: "+ resultado);
+Console.ReadLine(); */
+
+//Exercicio 2
+
+/*
+int num1 = 0;
+int num2 = 0;
+Console.Write("Digite o primeiro número: ");
+num1 = int.Parse(Console.ReadLine());
+Console.Write("Digite o segundo número: ");
+num2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Resultado soma: "+ (num1 + num2));
+Console.WriteLine("Resultado subtração: "+ (num1 - num2));
+Console.WriteLine("Resultado multiplicação: "+ (num1 * num2));
+Console.WriteLine("Resultado divisão: "+ ((double) num1 / num2)); */
+
+//Exercicio 3
+
+/*
+int num1 = 0;
+double raiz = 0;
+Console.Write("Digite o número: ");
+num1 = int.Parse(Console.ReadLine());
+raiz = Math.Sqrt(num1);
+Console.WriteLine("O resultado da raiz quadrada é: " + raiz); */
+
+//Exercicio 4
+
+/*
+int A, B;
+
+int aux = 0;
+Console.Write("Digite o número A: ");
+A = int.Parse(Console.ReadLine());
+Console.Write("Digite o número B: ");
+B = int.Parse(Console.ReadLine());
+aux = A;
+A = B;
+B = aux;
+Console.WriteLine("A = "+A);
+Console.WriteLine("B = "+B); */
+
+//Exercicio 5
+
+/*
+int TotalEleitores, VotoNulo, VotoValido, VotoBranco = 0;
+
+Console.Write("Digite o número de total de eleitores: ");
+TotalEleitores = int.Parse(Console.ReadLine());
+Console.Write("Digite o número de votos nulos: ");
+VotoNulo = int.Parse(Console.ReadLine());
+Console.Write("Digite o número de votos validos: ");
+VotoValido = int.Parse(Console.ReadLine());
+Console.Write("Digite o número de votos em branco: ");
+VotoBranco = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Porcentagem de votos nulos: "+ (((double)VotoNulo / TotalEleitores)* 100));
+Console.WriteLine("Porcentagem de votos validos: "+ (((double)VotoValido / TotalEleitores)*100));
+Console.WriteLine("Porcentagem de votos em branco: "+ (((double)VotoBranco / TotalEleitores)* 100));*/
+
+//Exercicio 6
